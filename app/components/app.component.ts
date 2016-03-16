38872b0056093d5846176bd34e70d949af4b1c41
Osama Sidat <oosidat@gmail.com>
2016-03-16 17:55:26 -0400

    experimenting with putting components in their own folders

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,8 +1,10 @@
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import {Component} from 'angular2/core';
 import {HeroService} from '../services/hero.service';
+
 import {HeroesComponent} from './heroes.component';
-import {DashboardComponent} from './dashboard.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
   selector: 'my-app',
@@ -26,6 +28,11 @@ import {DashboardComponent} from './dashboard.component';
     name: 'Dashboard',
     component: DashboardComponent,
     useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   },
 ])
 export class AppComponent {
--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from 'angular2/core';
-import { Hero } from '../models/hero';
-import { HeroService } from '../services/hero.service';
+import { Hero } from '../../models/hero';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: 'app/components/dashboard.component.html'
+  templateUrl: 'app/components/dashboard/dashboard.component.html'
 })
 export class DashboardComponent implements OnInit{
   heroes: Hero[] = [];
